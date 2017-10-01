bc6e8c3b005d0f4891f6b02daeeed169237b2de3
nestina <nestina@centrum.cz>
2017-10-01 19:11:18 +0200

    master
    master

---
+++ app/src/main/java/net/skero/czskinnl/AboutActivity.java
@@ -0,0 +1,14 @@
+package net.skero.czskinnl;
+
+import android.app.*;
+import android.os.*;
+
+public class AboutActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
