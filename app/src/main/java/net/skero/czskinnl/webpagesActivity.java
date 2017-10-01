05ef8e44c8182cce1c994d17a9b365e6f0aa4b48
nestina@centrum.cz <nestina@centrum.cz>
2017-10-01 23:36:59 +0200

    ok

--- app/src/main/java/net/skero/czskinnl/webpagesActivity.java
+++ app/src/main/java/net/skero/czskinnl/webpagesActivity.java
@@ -2,14 +2,19 @@ package net.skero.czskinnl;
 
 import android.app.*;
 import android.os.*;
+import android.webkit.WebView;
 
 public class webpagesActivity extends Activity 
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+		WebView w = new WebView(this);
+		
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.webpages);
+        setContentView(w);
+		
+		w.loadUrl("http://www.skero.net/");
     }
 
 
