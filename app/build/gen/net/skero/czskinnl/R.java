0562c549a620de8e41ca69d4e214b1fa7519caa1
nestina <nestina@centrum.cz>
2017-10-07 20:40:31 +0200

    ok

--- app/build/gen/net/skero/czskinnl/R.java
+++ app/build/gen/net/skero/czskinnl/R.java
@@ -15,10 +15,11 @@ public final class R {
     }
     public static final class layout {
         public static final int about=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int menu=0x7f030002;
-        public static final int start=0x7f030003;
-        public static final int webpages=0x7f030004;
+        public static final int forum=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int menu=0x7f030003;
+        public static final int start=0x7f030004;
+        public static final int webpages=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
